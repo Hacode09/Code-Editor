6d9565446609463a71026b88ed28de2a4caff4c3
Er.Rahul <h.raul0905@gmail.com>
2023-04-13 13:59:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function login() {
+    var uname = document.getElementById('first').value;
+    if (uname) {
+        location.assign('http://127.0.0.1:5500/home.html');
+    } else {
+        window.alert("Login faild");
+    }
+}
